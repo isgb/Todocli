0b6b4dafe74d4cd41d79fd81ca6ef50f91d99f35
oscar <oscar.vasquez@zentro.com.mx>
2024-03-07 16:53:53 -0600

    debug

---
+++ app.js
@@ -0,0 +1,42 @@
+import {createInterface} from "readline";
+import chalk from "chalk";
+
+const task = [];
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function displayMenu() {
+    console.log(chalk.redBright.bold("🦊🦊🦊🦊 To Do App 🦊🦊🦊🦊"));
+    console.log("1. Agregar tarea");
+    console.log("2. Listar tareas");
+    console.log("3. Completar tarea");
+    console.log("4. Salir");
+}
+
+function chooseOption(){
+    rl.question("Elige una opción, digita el número de tu opción:", (choice) => {
+        switch(choice){
+            case "1":
+                console.log("Creando Tarea");
+                break;
+            case "2":
+                console.log("Listando tareas");
+                break;
+            case "3":
+                console.log("Completar Tarea");
+                break;
+            case "4":
+                console.log(chalk.yellowBright("Adiós 🦊👋"));
+                rl.close()
+                break;
+            default:
+                console.log(chalk.red("Opcion invalida, Intenta neuvamente \n"));
+                break;
+        }
+    })
+}
+
+displayMenu();
